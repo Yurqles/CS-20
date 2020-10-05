e5f9dd1c255834b041d6aef1bf66e3c3216b021a
Yurqles <57115688+Yurqles@users.noreply.github.com>
2020-10-05 13:13:46 -0600

    Update main.js

--- Project Apply Concepts/main.js
+++ Project Apply Concepts/main.js
@@ -1,75 +1,76 @@
-//Platform Game and CS20 Project
+//Project
 
-// Set up Canvas and Graphics Context
+//Setup Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
-initGraphics(800, 600);
+// 40 rows and 34 columns
+initGraphics(640, 544)
 
 let map = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], ]
 
-//Ball
 let player = {
-    x: 0,
-    y: 0,
-    r : 7,
-    color: 'blue',
-    x_velocity: 0, //move x
-    y_velocity: 0, //move y
-    alive: true,
+    x: 50,
+    y: 447,
+    w: 32,
+    h: 32,
+    x_velocity: 0, // move x
+    y_velocity: 0, //move y 
+    alive : true,
+    jumping: false,
 }
-
-//Key Events
-document.addEventListener("keydown", controller.keyboard);
-document.addEventListener('keyup', controller.keyboard);
 
 let controller = {
     left: false,
-    right:false,
-    left: false,
-
+    right: false,
+    up: false,
     //Function named keyboard
     keyboard: function (event) {
 
         let key_state = (event.type == "keydown")
 
         switch (event.keyCode) {
-            case 65: // left key
+            case 65: // a key
                 controller.left = key_state;
                 break;
-            case 87: // up key
+            case 87: // w key
                 controller.up = key_state;
                 break;
-            case 68: // right key
+            case 68: // d key
                 controller.right = key_state;
                 break;
-            case 32: // right key
+            case 83: // s key
+                controller.down = key_state;
+            break;
         }
     }
-}
+};
 
-//Gravity
-player.y_velocity += 1.5;
+document.addEventListener("keydown", controller.keyboard);
+document.addEventListener('keyup', controller.keyboard);
 
-//Friction
-player.x_velocity *= 0.9;
-player.y_velocity *= 0.9;
+//Spritesheet 16 x 16
+let spriteSheetImg = document.getElementById('sprite-sheet');
+let spriteY = 0;
+let spriteX = 66;
+
 
 // Main Program Loop
 requestAnimationFrame(draw);
 
 function draw() {
     //Logic
+    // KEYBOARD CONTROLS
+    player.x += player.x_velocity;
+    player.y += player.y_velocity;
 
-    //Keyboard Controls
     if (controller.up && player.jumping == false) {
         player.y_velocity -= 20;
         player.jumping = true;
@@ -80,15 +81,37 @@ function draw() {
     if (controller.right) {
         player.x_velocity += 0.6;
     }
+    //If the keys are active these statements will make the player move
+
+    //Gravity
+    player.y_velocity += 1.3;
+
+    //Friction
+    player.x_velocity *= 0.9;
+    player.y_velocity *= 0.9;
+
+    //Make the player stay within the area
+    if (player.y > 447) {
+        player.y = 447;
+        player.jumping = false;
+    } 
+    if (player.x < -10) {
+        player.x = -10;
+    } else if (player.x > 630) {
+        player.x = 630;
+    }
 
     // Drawing
     ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-    //Draw ball
-    ctx.fillStyle = player.color;
-    ctx.fillCircle(player.x, player.y, player.r);
+    //Draw a ground
+    ctx.strokeStyle = 'black';
+    ctx.line(0, 480, 640, 480);
+    ctx.stroke()
+
+    //Draw the player
+    ctx.drawImage(spriteSheetImg, spriteX, spriteY, 16, 16, player.x, player.y, player.w, player.h)
 
     // Request another Animation Frame
     requestAnimationFrame(draw);
 }
-
